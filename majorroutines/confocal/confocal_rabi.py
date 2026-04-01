fe41a57fbe467fd1d5a5327fc92a5df554895823
chemistatcode <cmreiter221@gmail.com>
2026-04-01 02:12:11 -0700

    Rabi Pulse Fix

--- majorroutines/confocal/confocal_rabi.py
+++ majorroutines/confocal/confocal_rabi.py
@@ -161,7 +161,7 @@ def main(
 
     freq_ghz = uwave_freq_ghz if uwave_freq_ghz is not None else vsg["frequency"]
     sig_gen.set_freq(float(freq_ghz))
-    sig_gen.load_pulse_mod(5)  # Blank modulation with external TTL gate
+    sig_gen.mod_off()
     sig_gen.uwave_on()
 
     seq_file = "rabi.py"
