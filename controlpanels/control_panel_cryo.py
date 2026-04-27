fab4c738e167dd6e74a4dd127fe0ee72b41ccdd8
sarojchand <sarojc23@gmail.com>
2026-04-27 12:36:10 -0700

    Update control_panel_cryo.py

--- controlpanels/control_panel_cryo.py
+++ controlpanels/control_panel_cryo.py
@@ -1410,12 +1410,13 @@ if __name__ == "__main__":
         # do_optimize_xy(nv_sig, num_steps=8, scan_range=0.008) #xy galvo optimize but it works :)
         # do_optimize_xy_loop(nv_sig, num_iterations=3, num_steps=16, scan_range=0.008)
 
-        do_compensate_for_drift(nv_sig)
+        # do_compensate_for_drift(nv_sig)
         # do_optimize_galvo(nv_sig) # optimize xy for drift
         # do_optimize_z(nv_sig) # optimize z for drift
         # do_green_optimize_loop(nv_sig, num_iterations=3)  # Optimize before resonance scans to ensure we're on target
 
         #Optimize seq. parameters 
+        do_optimize_green_power(nv_sig)
         # do_optimize_green_readout_time(nv_sig)
         # do_find_apd_gate_overlap(nv_sig)
         # endregion Optimize
