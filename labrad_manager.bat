46cae7ec5bace408586a94fe0ab6954d3f0ab733
saroj chand <sarojc23@gmail.com>
2026-07-14 20:24:51 -0700

    labrad manager updates

--- labrad_manager.bat
+++ labrad_manager.bat
@@ -1,6 +1,30 @@
-:: This file may have to be tweaked for your specific PC. Just consider it a starting point. 
-start cmd /k ""C:\Users\Saroj Chand\Documents\dioptric\manager\scalabrad-0.8.3\bin\labrad.bat""
-start cmd /k ""C:\Users\Saroj Chand\Documents\dioptric\manager\scalabrad-web-server-2.0.5\bin\labrad-web.bat""
-start chrome /new-window http://localhost:7667
-call "C:\Users\Saroj Chand\miniconda3\Scripts\activate.bat" dioptric
-call python -m labrad.node -u "" -w ""
+@echo off
+
+REM Set your dioptric repo path
+set "DIOPTRIC_DIR=%USERPROFILE%\Github\dioptric"
+set "LABRAD_MANAGER=%DIOPTRIC_DIR%\labradmanager"
+
+REM Start LabRAD server
+start "LabRAD" cmd /k ""%LABRAD_MANAGER%\scalabrad-0.8.3\bin\labrad.bat""
+
+REM Start LabRAD web server
+start "LabRAD Web" cmd /k ""%LABRAD_MANAGER%\scalabrad-web-server-2.0.5\bin\labrad-web.bat""
+
+REM Open LabRAD web interface
+start "" chrome --new-window "http://localhost:7667"
+
+REM Activate conda environment
+if exist "%USERPROFILE%\miniconda3\condabin\conda.bat" (
+    call "%USERPROFILE%\miniconda3\condabin\conda.bat" activate dioptric
+) else if exist "C:\ProgramData\miniconda3\condabin\conda.bat" (
+    call "C:\ProgramData\miniconda3\condabin\conda.bat" activate dioptric
+) else (
+    echo Could not find conda.bat
+    pause
+    exit /b 1
+)
+
+REM Start LabRAD node
+python -m labrad.node -u "" -w ""
+
+pause
